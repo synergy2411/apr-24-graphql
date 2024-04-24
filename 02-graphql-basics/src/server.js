b74769d81716c6f8864223da8a9002bea5fe8d6e
Sumit Khandelwal <synergy2411@outlook.com>
2024-04-24 14:57:18 +0530

    search criteria added

--- 02-graphql-basics/src/server.js
+++ 02-graphql-basics/src/server.js
@@ -1,23 +1,65 @@
 import { createServer } from "node:http";
 import { createSchema, createYoga } from "graphql-yoga";
 
-// Scalar Types - ID, String, Int, Float, Boolean
+const users = [
+  { id: "u001", name: "Monica Geller", age: 22 },
+  { id: "u002", name: "Rachel Green", age: 24 },
+  { id: "u003", name: "Chandler Bing", age: 23 },
+];
 
-const typeDefs = `
-    type Query {
-        hello : String!
-        productId : ID!
-        quantity : Int!
-        isAvailable : Boolean!
-    }
+const posts = [
+  { id: "p001", title: "GraphQL 101", body: "for beginners", published: false },
+  {
+    id: "p002",
+    title: "Mastering GraphQL",
+    body: "for advanced developers",
+    published: true,
+  },
+  {
+    id: "p003",
+    title: "React for Beginners",
+    body: "frontend library",
+    published: false,
+  },
+  {
+    id: "p004",
+    title: "Mastering NodeJS",
+    body: "for server side scripting",
+    published: true,
+  },
+];
+
+const typeDefs = /* GraphQL */ `
+  type Query {
+    users(searchTerm: String): [User!]!
+    posts(search: String): [Post!]!
+  }
+  type User {
+    id: ID!
+    name: String!
+    age: Int!
+  }
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
+  }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "World!!",
-    productId: () => "P001",
-    quantity: () => 301,
-    isAvailable: () => true,
+    users: (parent, args, context, info) => {
+      if (args.searchTerm.trim() !== "") {
+        return users.filter((user) =>
+          user.name.toLowerCase().includes(args.searchTerm.toLowerCase())
+        );
+      }
+      return users;
+    },
+    posts: () => {
+      return posts;
+    },
   },
 };
 
