da5a027c2179d02c939630be28d68e76f30e6d84
Sumit Khandelwal <synergy2411@outlook.com>
2024-04-25 15:59:41 +0530

    prisma added

--- 03-graphql-prisma/src/server.js
+++ 03-graphql-prisma/src/server.js
@@ -3,17 +3,26 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const createdUser = await prisma.user.create({
-    data: {
-      name: "Monica Geller",
-      email: "monica@test.com",
-      password: "monica123",
-      age: 23,
-      role: "ANALYST",
+  const foundUser = await prisma.user.findUnique({
+    where: {
+      email: "xyz@test.com",
     },
   });
 
-  console.log("NEW ITEM : ", createdUser);
+  console.log(foundUser);
+
+  //   await prisma.user.deleteMany();
+  //   const createdUser = await prisma.user.create({
+  //     data: {
+  //       name: "Monica Geller",
+  //       email: "monica@test.com",
+  //       password: "monica123",
+  //       age: 23,
+  //       role: "ANALYST",
+  //     },
+  //   });
+
+  //   console.log("NEW ITEM : ", createdUser);
 }
 
 main()
