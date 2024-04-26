4f240d9a2cc870df57a599061648cbf4e974d6f8
Sumit Khandelwal <synergy2411@outlook.com>
2024-04-26 12:50:53 +0530

    posts fetched

--- frontend/src/Components/Posts/Posts.jsx
+++ frontend/src/Components/Posts/Posts.jsx
@@ -1,7 +1,37 @@
+import { gql, useQuery } from "@apollo/client";
+
+const FETCH_POSTS = gql`
+  query {
+    posts {
+      id
+      title
+      body
+      published
+      author {
+        id
+        name
+      }
+    }
+  }
+`;
+
 function Posts() {
+  const { data, loading, error } = useQuery(FETCH_POSTS);
+
+  if (error) return <p>{error.message}</p>;
+
+  if (loading) return <h1>Loading...</h1>;
+
   return (
     <>
-      <h1>Posts coming soon...</h1>
+      <h1 className="text-center">All Posts</h1>
+
+      <ul>
+        {data &&
+          data.posts.map((post) => (
+            <li key={post.id}>{post.title.toUpperCase()}</li>
+          ))}
+      </ul>
     </>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4040/graphql",
+  cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
